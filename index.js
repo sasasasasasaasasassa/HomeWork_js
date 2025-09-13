780770abd2b6aad0e8200bac234165a8c561d685
Sasha Istomin <sasaistomin42@gmail.com>
2025-09-13 21:47:02 +0300

    sdfxvc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+// 1. Подсчитать сумму всех чисел в заданном пользователем диапазоне.
+// let number_start = parseInt(prompt("Enter start number"));
+// let number_end = parseInt(prompt("Enter end number"));
+// let sum = 0;
+// for(let i = number_start; i <= number_end; i++) {
+//     sum += i;
+// }
+
+// 2. Запросить 2 числа и найти только наибольший общий делитель.
+dfsfsdgfd
+
+// 3. Запросить у пользователя число и вывести все делители этого числа.
+
+
+// 4. Определить количество цифр в введенном числе.
